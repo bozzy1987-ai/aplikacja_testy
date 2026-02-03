d8c6dbbad1fcc4d93cb1db444eac5859d103c2ea
Piotr Rybicki <p_rybicki@wp.pl>
2026-02-03 12:41:21 +0100

    Remove unused useState import to fix ESLint error

--- bike-shop/src/App.jsx
+++ bike-shop/src/App.jsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './App.css';
-import { useState } from 'react';
+
 
 const bikeData = [
   {
